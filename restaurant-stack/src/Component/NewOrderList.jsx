43204fa393d70e087403e334256ff2afb4745ac9
NicholasWarui <nyamunick99@gmail.com>
2023-06-16 12:54:29 +0300

    send order notification

--- restaurant-stack/src/Component/NewOrderList.jsx
+++ restaurant-stack/src/Component/NewOrderList.jsx
@@ -15,6 +15,17 @@ const NewOrderList = ({ id, selectedMenus }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if (orderItems.length === 0) {
+      // Display the notification
+      const notification = document.getElementById('alert-1');
+      notification.classList.remove('hidden');
+  
+      setTimeout(() => {
+        notification.classList.add('hidden');
+      }, 4000);
+  
+      return;
+    } else {
     const updatedOrders = {
       table_id: id,
       order_number: orderNumber,
@@ -22,8 +33,16 @@ const NewOrderList = ({ id, selectedMenus }) => {
         food_id: item.menuId,
         quantity: item.quantity,
       })),
-
     };
+  
+    //For dismissing the notification
+      const closeButton = document.querySelector('[data-target="#alert-1"]');
+      closeButton.addEventListener('click', () => {
+        const notification = document.getElementById('alert-1');
+        notification.classList.add('hidden');
+      });
+    
+
 
     setLoading(true);
     axiosClient.post('/orders', updatedOrders)
@@ -38,6 +57,7 @@ const NewOrderList = ({ id, selectedMenus }) => {
       
 
     console.log('Order submitted:', updatedOrders);
+    }
   };
 
 // Function for updating the quantity of an item in the order list
@@ -124,6 +144,25 @@ const NewOrderList = ({ id, selectedMenus }) => {
             {orderItems.map((orderItem) => (
                 <NewOrderItem key={orderItem.menuId} item={orderItem} onQuantityChange={handleQuantityChange} />
             ))}
+
+            {/* Display the notification */}
+            <div id="alert-1" class="flex p-4 mb-4 text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 hidden">
+              <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+              </svg>
+              <span class="sr-only">Info</span>
+              <div class="ml-3 text-sm font-medium">
+                Order items cannot be empty.
+              </div>
+              <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700" data-target="#alert-1" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                </svg>
+              </button>
+          </div>
+
+
         </div>
 
         <div className="total-container">
