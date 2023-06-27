a68a9c33f99c5220260eeb13ea10999881360936
richard9809 <richard.ndisya@strathmore.edu>
2023-06-27 15:43:26 +0300

    sidebar
    Signed-off-by: richard9809 <richard.ndisya@strathmore.edu>

--- restaurant-stack/src/Component/Sidebar.jsx
+++ restaurant-stack/src/Component/Sidebar.jsx
@@ -7,13 +7,14 @@ import axiosClient from '../axios-client';
 const Sidebar = ({logout}) => {
 
   return (
-    <div className="bg-gray-200 w-20 h-full flex flex-col justify-evenly">
+    <div className='bg-gray-200 w-20 h-full gap-20 flex flex-col justify-between'>
       <SidebarItem icon={<i className="fa fa-home" />} name="HOME" to="/" />
       <SidebarItem icon={<i className="fa fa-book" />} name="MENU" to="/menu" />
       <SidebarItem icon={<i className="fa fa-money" />} name="PAYMENT" to="/payment" />
       <SidebarItem icon={<i className="fa fa-grip" />} name="ORDERS" to="/orders" />
-      <SidebarItem icon={<i className="fa fa-cog" />} name="SETTINGS" />
-      <SidebarItem icon={<i className="fa fa-arrow-right-from-bracket" />} name="LOG OUT" onClick={logout}/>
+      <div className='border-t border-gray-400 py-4'>
+        <SidebarItem icon={<i className="fa fa-arrow-right-from-bracket" />} name="LOG OUT" onClick={logout}/>
+      </div>
     </div>
   );
 };
